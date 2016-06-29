24735b11d865e69e9655b672fff905a4d5495e9b
Ashwin Rao <theonlyrao@gmail.com>
2016-06-29 16:03:17 -0600

    Build search functionality

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -93,6 +93,15 @@ $(document).ready(function(){
 	}
     });
 
+    $("#search").keyup(function(){
+	var text = $(this).val();
+	var titleResults = $(".idea-list .card .card-title .idea-title:contains(" + text + ")").parent().parent();
+	var bodyResults = $(".idea-list .card .idea-body:contains(" + text + ")").parent();
+	$(".idea-list .card").hide();
+	titleResults.show();
+	bodyResults.show();
+    })
+
 
     function thumbsUp(currentQuality){
 	if (currentQuality === "swill"){
