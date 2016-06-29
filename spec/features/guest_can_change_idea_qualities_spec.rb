0cb15a235c2eb12afda36409add76709ecc8bd03
Ashwin Rao <theonlyrao@gmail.com>
2016-06-29 13:07:47 -0600

    Complete all tests for changing qualities

--- spec/features/guest_can_change_idea_qualities_spec.rb
+++ spec/features/guest_can_change_idea_qualities_spec.rb
@@ -12,18 +12,58 @@ RSpec.feature "GuestCanChangeIdeaQualities", type: :feature do
     end
   end
 
-  scenario "idea goes from plausible to skill" do
+  scenario "idea goes from plausible to swill", js: true do
+    @idea = create(:idea, title: "plausible idea", quality: "plausible")
+    visit root_path
+
+    within ".idea-list" do
+      find(".thumbs-down").click
+      expect(page).to have_content("plausible idea")
+      expect(page).to have_content("swill")
+    end
   end
 
-  scenario "idea goes from genius to plausible" do
+  scenario "idea goes from genius to plausible", js: true do
+    @idea = create(:idea, title: "genius idea", quality: "genius")
+    visit root_path
+
+    within ".idea-list" do
+      find(".thumbs-down").click
+      expect(page).to have_content("genius idea")
+      expect(page).to have_content("plausible")
+    end
   end
 
-  scenario "idea goes from swill to plausible" do
+  scenario "idea goes from swill to plausible", js: true do
+    @idea = create(:idea, title: "swill idea", quality: "swill")
+    visit root_path
+
+    within ".idea-list" do
+      find(".thumbs-up").click
+      expect(page).to have_content("swill idea")
+      expect(page).to have_content("plausible")
+    end
   end
 
-  scenario "idea stays on swill" do
+  scenario "idea stays on swill", js: true do
+    @idea = create(:idea, title: "swill idea", quality: "swill")
+    visit root_path
+
+    within ".idea-list" do
+      find(".thumbs-down").click
+      expect(page).to have_content("swill idea")
+      expect(page).to have_content("swill")
+    end
   end
 
-  scenario "idea stays on genius" do
+  scenario "idea stays on genius", js: true do
+    @idea = create(:idea, title: "genius idea", quality: "genius")
+    visit root_path
+
+    within ".idea-list" do
+      find(".thumbs-up").click
+      expect(page).to have_content("genius idea")
+      expect(page).to have_content("genius")
+    end
   end
 end
