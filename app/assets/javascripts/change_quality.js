3715434f45ac4547f3f8a9b3abf259f9b3f396f4
Ashwin Rao <theonlyrao@gmail.com>
2016-06-29 21:26:04 -0600

    Refactor quality functions to own file

---
+++ app/assets/javascripts/change_quality.js
@@ -0,0 +1,53 @@
+function changeQualityListener(){
+    $(".idea-list").delegate(".thumbs-up", "click", function(){
+	var ideaId = $(this).parent().data("id");
+	var currentQuality = $(this).parent().find("#quality").text()
+	var newQuality = thumbsUp(currentQuality)
+	$(this).parent().find("#quality").text(newQuality)
+	$.ajax({
+	    type: "PATCH",
+	    url: "api/v1/ideas/" + ideaId,
+	    data: { idea: { quality: newQuality } },
+	    dataType: "json",
+	    success: null
+	})
+    })
+
+    $(".idea-list").delegate(".thumbs-down", "click", function(){
+	var ideaId = $(this).parent().data("id");
+	var currentQuality = $(this).parent().find("#quality").text()
+	var newQuality = thumbsDown(currentQuality)
+	$(this).parent().find("#quality").text(newQuality)
+	$.ajax({
+	    type: "PATCH",
+	    url: "api/v1/ideas/" + ideaId,
+	    data: { idea: { quality: newQuality } },
+	    dataType: "json",
+	    success: null
+	})
+    })
+}
+
+function thumbsUp(currentQuality){
+    if (currentQuality === "swill"){
+	return "plausible";
+    }
+    else if (currentQuality === "plausible"){
+	return "genius";
+    }
+    else {
+	return currentQuality;
+    }
+}
+
+function thumbsDown(currentQuality){
+    if (currentQuality === "genius"){
+	return "plausible";
+    }
+    else if (currentQuality === "plausible"){
+	return "swill";
+    }
+    else {
+	return currentQuality;
+    }
+}
--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,34 +2,7 @@ $(document).ready(function(){
     getIdeas();
     saveIdeaListener();
     deleteIdeaListener();
-
-    $(".idea-list").delegate(".thumbs-up", "click", function(){
-	var ideaId = $(this).parent().data("id");
-	var currentQuality = $(this).parent().find("#quality").text()
-	var newQuality = thumbsUp(currentQuality)
-	$(this).parent().find("#quality").text(newQuality)
-	$.ajax({
-	    type: "PATCH",
-	    url: "api/v1/ideas/" + ideaId,
-	    data: { idea: { quality: newQuality } },
-	    dataType: "json",
-	    success: null
-	})
-    })
-
-    $(".idea-list").delegate(".thumbs-down", "click", function(){
-	var ideaId = $(this).parent().data("id");
-	var currentQuality = $(this).parent().find("#quality").text()
-	var newQuality = thumbsDown(currentQuality)
-	$(this).parent().find("#quality").text(newQuality)
-	$.ajax({
-	    type: "PATCH",
-	    url: "api/v1/ideas/" + ideaId,
-	    data: { idea: { quality: newQuality } },
-	    dataType: "json",
-	    success: null
-	})
-    })
+    changeQualityListener();
 
     var contents = $(".idea-list").html();
     $(".idea-list").delegate(".idea-title", "blur", function() {
@@ -69,29 +42,5 @@ $(document).ready(function(){
 	bodyResults.show();
     })
 
-
-    function thumbsUp(currentQuality){
-	if (currentQuality === "swill"){
-	    return "plausible";
-	}
-	else if (currentQuality === "plausible"){
-	    return "genius";
-	}
-	else {
-	    return currentQuality;
-	}
-    }
-
-    function thumbsDown(currentQuality){
-	if (currentQuality === "genius"){
-	    return "plausible";
-	}
-	else if (currentQuality === "plausible"){
-	    return "swill";
-	}
-	else {
-	    return currentQuality;
-	}
-    }
 });
 
