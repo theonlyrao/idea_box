5564b7afcb58e6c18d285eea087065db80f67fe6
Ashwin Rao <theonlyrao@gmail.com>
2016-06-29 21:16:49 -0600

    Refactor get ideas to own file

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,11 +1,6 @@
 $(document).ready(function(){
-    $.ajax({
-	method: "GET",
-	url: "/api/v1/ideas",
-	dataType: "json",
-	success: displayIdeas
-    })
-
+    getIdeas();
+    
     $("#save-button").click(function(){
 	var newTitle = $("#title").val();
 	var newBody = $("#body").val();
@@ -126,16 +121,6 @@ $(document).ready(function(){
 	    return currentQuality;
 	}
     }
-
-    function displayIdea(idea){
-	$(".idea-list").prepend("<div class='card card-block' id=" + idea.id + " data-id=" + idea.id + "><h4 class='card-title'><div class=idea-title contenteditable='true'>" + idea.title + "</div><div id=quality>"  + idea.quality + "</div></h4><p class='card-text idea-body' contenteditable='true'>" + idea.body + "</p><a class='card-link delete'>Delete</a><a class='card-link thumbs-up'>Thumbs Up</a><a class='card-link thumbs-down'>Thumbs Down</a></div>")
-    };
-
-    function displayIdeas(ideas) {
-	ideas.forEach(function(idea){
-	    displayIdea(idea);
-	});
-    };
 
     function deleteIdea(ideaId){
 	$(".card#" + ideaId).remove();
